dabbb2b78e6e0164c0b40aa7dfd219e3765e9dd2
Gustavo CM <gustavo.alocoma@gmail.com>
2023-07-04 13:47:16 -0500

    initial commit

---
+++ apps/client/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  let message = 'No message';
+  const fetchApi = async () => {
+    const raw = await fetch('/api');
+    const data = await raw.json();
+    message = data.msg;
+  };
+</script>
+
+<main>
+  <h1>Muni Chen Chen</h1>
+
+  <div class="card">
+    <button on:click={fetchApi}>Click me!</button>
+    <p><b>{message}</b></p>
+  </div>
+</main>
